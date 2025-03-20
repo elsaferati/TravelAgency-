6e4601f77dfe0a390e110483e6a8ed88787bb4ca
Vjona <lekajvjona@hotmail.com>
2025-03-20 23:34:16 +0100

    Update login.php
    code for login

--- login.php
+++ login.php
@@ -24,4 +24,33 @@ if (isset($_POST['loginbtn'])) {
         echo "Incorrect Username or Password!";
     }
 }
-?>  $password = $_POST['password'];
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Overpass:wght@300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Croissant+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/log-in-style.css">
+    <link rel="stylesheet" href="styles/shared.css"> 
+    <link rel="stylesheet" href="styles/header.css">
+    <script src="script/header.js" type="text/javascript"></script>
+
+    <title>Log in</title>
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <div class="logo-image-container">
+                <img class="logo" src="images/2(1).png" alt="Logo" />
+            </div>
+            <div class="logo-text-container">
+                <p id="title"><a href="index.html"><strong>W A V E</strong></a></p>
+                <p id="under-title">Do More Than Travel</p>
+            </div>
+        </div>
