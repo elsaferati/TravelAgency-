19b4ccd3e2e0c5f2ab45a56f084d6473f6d31216
elsaferati <elsavferati@gmail.com>
2025-03-28 02:46:06 +0100

    create deleteBooking.php

---
+++ deleteBooking.php
@@ -0,0 +1,26 @@
+<?php
+// Database connection
+$connection = mysqli_connect('localhost', 'root', '', 'bookings');
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if ID is provided
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Delete query
+    $query = "DELETE FROM bookings WHERE id = $id";
+
+    if (mysqli_query($connection, $query)) {
+        echo "<script>alert('Booking deleted!'); window.location.href='bookinsInfo.php';</script>";
+    } else {
+        echo "Error deleting booking: " . mysqli_error($connection);
+    }
+} else {
+    echo "Invalid request!";
+}
+
+mysqli_close($connection);
+?>
