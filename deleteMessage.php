55edf58fb575a204e3266d6ac5d32f283a5233b9
elsaferati <elsavferati@gmail.com>
2025-03-29 00:37:19 +0100

    update deleteMessage.php

--- deleteMessage.php
+++ deleteMessage.php
@@ -1,28 +1,15 @@
 <?php
-// Database connection
-$connection = mysqli_connect('localhost', 'root', '', 'userinfo');
+require_once 'contactDatabase.php';
+require_once 'contactMessages.php';
 
-// Check connection
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Get message ID from URL
-$id = isset($_GET['id']) ? $_GET['id'] : '';
-
-if ($id) {
-    // Delete the message from the database
-    $query = "DELETE FROM `userdata` WHERE `id` = '$id'";
-    $result = mysqli_query($connection, $query);
-
-    if ($result) {
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $messageObj = new Message();
+    if ($messageObj->delete($id)) {
         echo "<script>alert('Message deleted successfully!'); window.location.href='contact-messages.php';</script>";
     } else {
         echo "Error: " . mysqli_error($connection);
     }
-} else {
-    echo "Invalid request.";
 }
-
-mysqli_close($connection);
 ?>
+
