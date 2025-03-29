e6f05c61bbf56e58ef89a1826e051d5cd6ceb9b9
Vjona <lekajvjona@hotmail.com>
2025-03-29 03:56:37 +0100

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE logindata;
+
+USE logindata;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('user', 'admin') DEFAULT 'user' NOT NULL
+);
