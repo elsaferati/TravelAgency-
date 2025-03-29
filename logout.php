7815215537cba3cee5893d7a899ceae5a18494cf
Vjona <lekajvjona@hotmail.com>
2025-03-29 04:02:35 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
-  session_start();
-
-  session_destroy();
-
-  header("location:log-in.php");
+session_start();
+session_unset();
+session_destroy();
+header("Location: log-in.php");
+exit();
 ?>
