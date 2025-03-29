f7cc1f385727f7cf9d61a4970779995dd9b01a04
deabahtirii <deabahtiri.it@gmail.com>
2025-03-29 02:49:50 +0100

    added some pictures

--- configV.php
+++ configV.php
@@ -5,8 +5,9 @@ $host = "localhost";
 $username = "root";
 $password = "";
 $database = "logindata";
+$port = 3307;
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database , $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
