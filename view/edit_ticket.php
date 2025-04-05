7aa3c2418e40de50cf357940dd88ba8d23ad82b3
zanabajrami <zb67471@ubt-uni.net>
2025-04-05 02:13:55 +0200

    fixed an error

--- view/edit_ticket.php
+++ view/edit_ticket.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "../config/ticketdatabase";
+$dbname = "ticket_booking";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
