0ec29e583835c3218d05ea60386168dcffef85f7
elsaferati <elsavferati@gmail.com>
2025-04-05 13:46:35 +0200

    updates

--- view/sweden.php
+++ view/sweden.php
@@ -56,8 +56,8 @@
                         <li><a href="sweden.php">Sweden</a></li>
                         <li><a href="italy.php">Italy</a></li>
                         <li><a href="uk.php">UK</a></li>
-                        <li><a href="greece.html">Greece</a></li>
-                        <li><a href="spain.html">Spain</a></li>
+                        <li><a href="greece.php">Greece</a></li>
+                        <li><a href="spain.php">Spain</a></li>
                     </ul>
                 </div>
                 </a>
